918ce8f5f70a0fd19d2ca80ec868ab58c201342e
vedikadurai <vedikadurai608@gmail.com>
2025-10-25 19:08:04 +0530

    Delete protected.js

--- protected.js
+++
@@ -1,8 +0,0 @@
-const express= require("express");
-const { loggedInPage} = require("../controllers/loggedin");
-const {verifyUser} = require('../authenticate');
-const router = express.Router();
-
-router.get("/index",verifyUser,loggedInPage);
-
-module.exports =router;
